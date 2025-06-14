d321aa3c00e913254a24f0ca97ca1ea1ccc0d2d7
tmnzaa <polosanpes818@gmail.com>
2025-06-15 01:21:28 +0700

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -1,10 +1,2 @@
 // maintenance.js
-const isMaintenance = localStorage.getItem("maintenanceMode") === "on";
-if (isMaintenance && !location.href.includes("admin.html")) {
-  document.body.innerHTML = `
-    <div style="display: flex; height: 100vh; align-items: center; justify-content: center; flex-direction: column; background: #121212; color: white; font-family: sans-serif; text-align: center;">
-      <h1 style="font-size: 24px;">Website sedang dalam perbaikan</h1>
-      <p style="color: #ccc;">Silakan kembali lagi nanti.</p>
-    </div>
-  `;
-}
+const MAINTENANCE_MODE = false;
